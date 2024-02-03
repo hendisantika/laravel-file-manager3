1ec63a9ce7c1f585f456a12114a1c3af283b6ae4
hendisantika <hendisantika@yahoo.co.id>
2024-02-04 06:37:16 +0700

    Add store in RegisteredUserController.php 🫸🌀✏️📗🐧🐳

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\Role;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Request;
+
 class RegisteredUserController extends Controller
 {
-    //
+    public function store(Request $request): RedirectResponse
+    {
+        // ...
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role_id' => Role::CUSTOMER->value,
+        ]);
+
+        // ...
+    }
 }
