6b1bf24980dbe774d0e299e912593a3dd4a73101
hendisantika <hendisantika@yahoo.co.id>
2024-02-04 06:43:23 +0700

    Modify StoreCompanyRequest.php 🫸🌀✏️📗🐧🐳

--- app/Http/Requests/StoreCompanyRequest.php
+++ app/Http/Requests/StoreCompanyRequest.php
@@ -12,7 +12,7 @@ class StoreCompanyRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +23,7 @@ class StoreCompanyRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string'],
         ];
     }
 }
