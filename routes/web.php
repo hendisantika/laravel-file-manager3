ffb20d108825eab3e430642c8530edfc2389f4cd
hendisantika <hendisantika@yahoo.co.id>
2024-02-04 06:38:27 +0700

    Add profile route 🫸🌀✏️📗🐧🐳

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +30,11 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+
+    // ...
+
+    Route::resource('companies', CompanyController::class);
+});
