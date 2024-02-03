a339b8ab736d35e5987105bc39a8671a0ca3aaf5
hendisantika <hendisantika@yahoo.co.id>
2024-02-04 06:41:32 +0700

    Add index.blade.php 🫸🌀✏️📗🐧🐳

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\View;
+
 class CompanyController extends Controller
 {
-    //
+    public function index(): View
+    {
+        $companies = Company::all();
+
+        return view('companies.index', compact('companies'));
+    }
 }
